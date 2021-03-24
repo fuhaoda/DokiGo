923f3bbdb3d321172f41d6f1a9b22d14e6e7fa78
fuhaoda <fu_haoda@lilly.com>
2021-03-24 18:22:01 -0400

    add self play MCTS

--- dokigo/encoders/base.py
+++ dokigo/encoders/base.py
@@ -26,6 +26,6 @@ class Encoder:
 def get_encoder_by_name(name, board_size):
     if isinstance(board_size, int):
         board_size = (board_size, board_size)
-    module = importlib.import_module('dokigo.encoder.' + name)
+    module = importlib.import_module('dokigo.encoders.' + name)
     constructor = getattr(module, 'create')
     return constructor(board_size)
--- dokigo/utilities.py
+++ dokigo/utilities.py
@@ -1,5 +1,5 @@
 from dokigo import base
-
+import numpy as np
 
 COLS = 'ABCDEFGHJKLMNOPQRST'
 
@@ -39,3 +39,19 @@ def point_from_coords(coords):
 def coords_from_point(point):
     return '%s%d'%(COLS[point.col-1], point.row)
 
+
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones((board.num_rows, board.num_cols))
+
+    def get(self, row, col):
+        return self.move_ages[row, col]
+
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = -1
+
+    def add(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
